ee20292d1bc91527fe77b70e2628cbe7b2071bf3
AndreasFagschlunger <andreas@fagschlunger.co.at>
2018-06-07 22:10:33 +0200

    Display CDM Status

--- projects/operator/src/lib/cdm/cdm-status/cdm-status.component.ts
+++ projects/operator/src/lib/cdm/cdm-status/cdm-status.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { CdmStatus320 } from 'xfs320';
+import { AntiFraudModule, DevicePosition, CdmGuidLights, Position, Shutter, PositionStatus, Transport, TransportStatus, IntermediateStacker, Dispenser, SafeDoor, CdmDeviceState } from 'xfs';
 
 @Component({
   selector: 'lib-cdm-status',
@@ -14,5 +15,34 @@ export class CdmStatusComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.status = {
+      deviceState: CdmDeviceState.ONLINE,
+      safeDoor: SafeDoor.CLOSED,
+      dispenser: Dispenser.OK,
+      intermediateStacker: IntermediateStacker.EMPTY,
+      positions: [
+        {
+          position: Position.FRONT,
+          shutter: Shutter.CLOSED,
+          positionStatus: PositionStatus.EMPTY,
+          transport: Transport.OK,
+          transportStatus: TransportStatus.EMPTY
+        }
+      ],
+      extra: {
+        'Key 1': 'Value 1',
+        'Key 2': 'Value 2'
+      },
+      guidLights: [
+        [ CdmGuidLights.MEDIUM_FLASH, CdmGuidLights.GREEN ]
+      ],
+      devicePosition: DevicePosition.INPOSITION,
+      powerSaveRecoveryTime: 2000,
+      antiFraudModule: AntiFraudModule.OK
+    };
+  }
+
+  extraKeys(): string[] {
+    return Object.getOwnPropertyNames(this.status.extra);
   }
 }
