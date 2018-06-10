2c32aba800b83717ccb804a8dca64a1c8ed66823
AndreasFagschlunger <andreas@fagschlunger.co.at>
2018-06-10 23:24:57 +0200

    Added CdmDeviceStateComponent

---
+++ projects/xfs/src/lib/cdm/cdm-device-state.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CdmDeviceState } from '.';
+
+@Component({
+  selector: 'cdmDeviceState',
+  templateUrl: './cdm-device-state.component.html'
+})
+export class CdmDeviceStateComponent {
+
+  @Input()
+  value: CdmDeviceState;
+}
--- projects/xfs/src/lib/xfs.module.ts
+++ projects/xfs/src/lib/xfs.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { XfsComponent } from './xfs.component';
 import { DeviceStateDirective } from './cdm/device-state.directive';
+import { CdmDeviceStateComponent } from './cdm/cdm-device-state.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
+    CommonModule
   ],
-  declarations: [XfsComponent, DeviceStateDirective],
-  exports: [XfsComponent, DeviceStateDirective ]
+  declarations: [XfsComponent, CdmDeviceStateComponent, DeviceStateDirective],
+  exports: [XfsComponent, CdmDeviceStateComponent, DeviceStateDirective ]
 })
 export class XfsModule { }
